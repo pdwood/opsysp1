cf812efcef82e56caea7a060610346349cbcc3b2
fawcek <fawcek@rpi.edu>
2017-03-08 22:39:13 -0500

    Fixed the compiling issues, WITHOUT commenting everything out.

--- Project1.java
+++ Project1.java
@@ -20,17 +20,17 @@ public class Project1 {
 	private static int contextSwitchTime = 8;
 	private static int timeToNextEvent;
 	private static int currentTime;
-
+	
 	private static boolean shouldPreempt(){ return false; } //TODO
 	private static void contextSwitch() {} //TODO
-
+	
 	public static void main(String[] args) {
-
-
+		
+		
 		queue = new PriorityQueue<Process>();
 		io = new PriorityQueue<Process>(11, new Comparator<Process>(){
 			public int compare(Process a, Process b){
-				return a.getRemainingTime() - b.getRemainingTime();
+				return a.getRemainingCPUTime() - b.getRemainingCPUTime();
 			}
 		});
 		//Outside should be queue sorted by arrival time
@@ -41,21 +41,21 @@ public class Project1 {
 		});
 		
 		parseFile(args[0]);
-
+		
 		currentTime=0;
-
+		
 		while(true){
-
+		
 			//TODO Account for context switch!
-
+			
 			if(outside.peek()!=null && outside.peek().getArrivalTime() == currentTime){
 				queue.add(outside.poll());
-			}		
-
-			if(currentProcess == null || currentProcess.getRemainingTime() == 0 || shouldPreempt()){
+			}
+			
+			if(currentProcess == null || currentProcess.getRemainingCPUTime() == 0 || shouldPreempt()){
 				contextSwitch();
 			}
-
+			
 			int timeDelta = queryNextEvent();
 			if(timeDelta == Integer.MAX_VALUE) break;
 		}
@@ -66,17 +66,17 @@ public class Project1 {
 	 */
 	private static int queryNextEvent(){
 		//(May be difficult for SRT...)
-
+		
 		//Possible next events: Outside arrival, process finishing CPU, process finishing IO, others? ... SRT preemption, but that is weird.
 		int timeDelta=Integer.MAX_VALUE;
 		if(outside.size() > 0) timeDelta = outside.peek().getArrivalTime() - currentTime;
-		if(currentProcess != null && currentProcess.getRemainingTime() < timeDelta) timeDelta = currentProcess.getRemainingTime();
-		if(io.size() > 0 && io.peek().getRemainingTime() < timeDelta) timeDelta = io.peek().getRemainingTime();
+		if(currentProcess != null && currentProcess.getRemainingCPUTime() < timeDelta) timeDelta = currentProcess.getRemainingCPUTime();
+		if(io.size() > 0 && io.peek().getRemainingCPUTime() < timeDelta) timeDelta = io.peek().getRemainingCPUTime();
 		if(cooldown > 0 && cooldown < timeDelta) timeDelta = cooldown;
 		
 		return timeDelta;
 	}
-
+	
 	/**
 	 * Updates the current times, and the remaining times of every process in IO or CPU
 	 */
@@ -86,8 +86,8 @@ public class Project1 {
 		currentTime += timeDelta;
 		if(cooldown >= timeDelta) cooldown -= timeDelta;
 	}
-
-	/**
+	
+	/*
 	 * Parse process data from a file, create new processes with the data.
 	 * Push new processes to the Outside queue.
 	 * Expected file format: <proc-id>|<initial-arrival-time>|<cpu-burst-time>|<num-bursts>|<io-time>
@@ -118,8 +118,8 @@ public class Project1 {
 		if(tokens.length != 5) throw new IllegalArgumentException("Invalid process description ("+tokens.length+"): "+in);
 		return new Process(tokens[0], Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]), Integer.parseInt(tokens[4]));
 	}
+	
 	/*
-	/**
 	 * If the cpu is empty, do nothing.
 	 * If the cpu is on cooldown, reduce cooldown.
 	 * --If the cooldown is complete, clear the cooldown and move process or start the burst
@@ -128,7 +128,7 @@ public class Project1 {
 	 * 
 	 * Update timeToNextEvent if necessary.
 	 * @param elapsedTime the amount of time since the last cpu update
-	 *
+	 */
 	private static void updateCPU(int elapsedTime){
 		
 		
@@ -138,7 +138,7 @@ public class Project1 {
 	 * Reduce remaining IO time for all processes in IO.
 	 * --If any processes are finished with IO, move them to the queue
 	 * @param elapsedTime the amount of time since the last cpu update
-	 *
+	 */
 	private static void updateIO(){
 		Iterator<Process> iter;
 		boolean debugging = false;
@@ -148,7 +148,7 @@ public class Project1 {
 			iter = io.iterator();
 			while (iter.hasNext()) {
 				Process p = iter.next();
-				System.out.print(p.getIOExitTime()+", ");
+				System.out.print(p.getNextStateChange()+", ");
 			}
 			System.out.println("");
 		}
@@ -156,7 +156,7 @@ public class Project1 {
 		iter = io.iterator();
 		while (iter.hasNext()) {
 			Process p = iter.next();
-			if (p.getIOExitTime() <= currentTime) {
+			if (p.getNextStateChange() <= currentTime) {
 				queue.add(p);
 				iter.remove();
 			}
@@ -167,7 +167,7 @@ public class Project1 {
 			iter = io.iterator();
 			while (iter.hasNext()) {
 				Process p = iter.next();
-				System.out.print(p.getIOExitTime()+", ");
+				System.out.print(p.getNextStateChange()+", ");
 			}
 			System.out.println("");
 		}
@@ -177,7 +177,7 @@ public class Project1 {
 	/**
 	 * Move processes to the queue if it is time for the process to enter.
 	 * @param elapsedTime the amount of time since the last cpu update
-	 *
+	 */
 	private static void updateOutside(){
 		Iterator<Process> iter;
 		boolean debugging = false;
@@ -218,9 +218,8 @@ public class Project1 {
 	 * Check for preemption
 	 * --Initiate context switch if necessary
 	 * @param elapsedTime the amount of time since the last cpu update
-	 *
+	 */
 	private static void updateQueue(){
 		
 	}
-	*/
 };
