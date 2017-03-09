62d434a2eea2f036ab432e49db1035c9b145ec65
fawcek <fawcek@rpi.edu>
2017-03-09 11:55:39 -0500

    Half a step to a nearby stable. Still sorting through main.

--- project1/Project1.java
+++ project1/Project1.java
@@ -1,3 +1,12 @@
+/*
+	CSCI 4210 Operating Systems Project 1
+	
+	Made by:
+	Kyle Fawcett fawcek
+	Peter Wood woodp
+	Gavin Petilli petilg
+*/
+
 package project1;
 
 import java.io.File;
@@ -52,17 +61,29 @@ public class Project1 {
 		while(true){
 			//TODO Account for context switch!
 			
+			/* This code is being removed as it is already being done in the 
+			 * updateOutside() function. 
 			if(outside.peek()!=null && outside.peek().getArrivalTime() == currentTime){
 				queue.add(outside.poll());
-			}
+			} */
 			
-			if(currentProcess == null || currentProcess.getRemainingCPUTime() == 0 || shouldPreempt()){
+			/* Removed the condition "currentProcess == null", because
+			 * sometimes the processor will be empty for a while, while 
+			 * everything is off doing IO, and it shouldn't switch. */
+			if(currentProcess.getRemainingCPUTime() == 0 || shouldPreempt()){
 				contextSwitch();
 			}
 			
 			int timeDelta = queryNextEvent();
 			if(timeDelta == Integer.MAX_VALUE) break;
 			updateTimestamps(timeDelta);
+			
+			/* Go through all the sectors and update/check their statuses. 
+			 * Yes, order does matter, see Piazza "Several Questions". */
+			updateCPU(timeDelta);
+			updateIO();
+			updateOutside();
+			
 		}
 	}
 	
@@ -71,13 +92,29 @@ public class Project1 {
 	 */
 	private static int queryNextEvent(){
 		//(May be difficult for SRT...)
+		int timeDelta=Integer.MAX_VALUE;
 		
 		//Possible next events: Outside arrival, process finishing CPU, process finishing IO, others? ... SRT preemption, but that is weird.
-		int timeDelta=Integer.MAX_VALUE;
-		if(outside.size() > 0) timeDelta = outside.peek().getArrivalTime() - currentTime;
-		if(currentProcess != null && currentProcess.getRemainingCPUTime() < timeDelta) timeDelta = currentProcess.getRemainingCPUTime();
-		if(io.size() > 0 && io.peek().getRemainingCPUTime() < timeDelta) timeDelta = io.peek().getRemainingCPUTime();
-		if(cooldown > 0 && cooldown < timeDelta) timeDelta = cooldown;
+		if(outside.size() > 0) {
+			timeDelta = outside.peek().getArrivalTime() - currentTime;
+		}
+		if(currentProcess != null && currentProcess.getRemainingCPUTime() < timeDelta) {
+			timeDelta = currentProcess.getRemainingCPUTime();
+		}
+		if(cooldown > 0 && cooldown < timeDelta) {
+			timeDelta = cooldown;
+		}
+		// Changed this one to timestamp as opposed to relative time, as IO is 
+		// based off of timestamps.
+		int nextTimeState = Integer.MAX_VALUE;
+		if(io.size() > 0 && io.peek().getNextStateChange() < nextTimeState) {
+			nextTimeState = io.peek().getNextStateChange();
+		}
+		// Then goes backwards and calculates timeDelta based off of timestamp.
+		// (Max value condition to ensure main exiting status. 
+		if (nextTimeState-currentTime < timeDelta && nextTimeState != Integer.MAX_VALUE) {
+			timeDelta = nextTimeState - currentTime;
+		}
 		
 		return timeDelta;
 	}
@@ -86,9 +123,11 @@ public class Project1 {
 	 * Updates the remaining times of process in CPU
 	 */
 	private static void updateTimestamps(int timeDelta){
-		if(currentProcess != null) currentProcess.decrementTime(timeDelta);
+		/* These line is getting removed, as it belongs to the CPU 
+		 * specifically, not the timestamps in general. 
+		 * if(currentProcess != null) currentProcess.decrementTime(timeDelta);
+		 * if(cooldown >= timeDelta) cooldown -= timeDelta; */
 		currentTime += timeDelta;
-		if(cooldown >= timeDelta) cooldown -= timeDelta;
 	}
 	
 	/**
@@ -141,9 +180,8 @@ public class Project1 {
 	}
 	
 	/**
-	 * Reduce remaining IO time for all processes in IO.
+	 * Check all processes to see if they are done doing IO. 
 	 * --If any processes are finished with IO, move them to the queue
-	 * @param elapsedTime the amount of time since the last cpu update
 	 */
 	private static void updateIO(){
 		Iterator<Process> iter;
